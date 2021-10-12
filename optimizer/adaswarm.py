a45a0db00cd28ffa5b8fe6108c0d369b9eff1702
swhustla <fdkelly@gmail.com>
2021-10-12 11:31:47 +0100

    remove edits to adaswarm.py

--- optimizer/adaswarm.py
+++ optimizer/adaswarm.py
@@ -1,9 +1,6 @@
-import optimizer.adaswarm as Adaswarm
-
-optimizer = Adaswarm(net.parameters(), lr=args.lr)
 
 def train():
     # Pass targets from dataset
     # outputs , net
     # call adaswarm optimiser.step
-    None
+    return
