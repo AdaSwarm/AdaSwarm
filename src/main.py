80f7ae5003cc0838e2992424c48dd6d36408c40b
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2021-10-28 15:17:02 +0100

    Optimize imports for DataLoader and DataParallel

--- src/main.py
+++ src/main.py
@@ -12,6 +12,8 @@ import torch
 from torch import nn
 from torch import optim
 from torch.backends import cudnn
+from torch.utils.data import DataLoader
+from torch.nn.parallel import DataParallel
 
 from resnet import ResNet18
 from utils import progress_bar
@@ -65,23 +67,19 @@ def run():
     trainset = torchvision.datasets.MNIST(
         root="./data", train=True, download=True, transform=transform_train
     )
-    trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=125, shuffle=True, num_workers=2
-    )
+    trainloader = DataLoader(trainset, batch_size=125, shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.MNIST(
         root="./data", train=False, download=True, transform=transform_test
     )
-    testloader = torch.utils.data.DataLoader(
-        testset, batch_size=100, shuffle=False, num_workers=2
-    )
+    testloader = DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
     # Model
     print("==> Building model..")
     net = ResNet18(1)
     net = net.to(DEVICE)
     if DEVICE == "cuda":
-        net = torch.nn.DataParallel(net)
+        net = DataParallel(net)
         cudnn.benchmark = True
 
     if args.resume:
