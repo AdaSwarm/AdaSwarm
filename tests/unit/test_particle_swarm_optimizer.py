cb00126189dd286729a192ce0adec4f113430e75
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2021-11-23 10:01:43 +0000

    Move average of acceleration coefficients to swarm

--- tests/unit/test_particle_swarm_optimizer.py
+++ tests/unit/test_particle_swarm_optimizer.py
@@ -1,4 +1,5 @@
 import unittest
+from numpy.core.fromnumeric import size
 from torch import randint, device as torch_device, Tensor
 from adaswarm.rempso import RotatedEMParticleSwarmOptimizer
 from adaswarm.particle import AccelerationCoefficients
@@ -21,11 +22,12 @@ class TestRotatedEMParticleSwarmOptimizer(unittest.TestCase):
             targets=np.argmax(targets, axis=1),
             dimension=dimension,
             number_of_classes=number_of_classes,
+            swarm_size=6,
             acceleration_coefficients=AccelerationCoefficients(c_1=0.8, c_2=0.9),
         )
 
-        c1r1_c2r2_sum, gbest = optimizer.run_iteration(number=5)
-        self.assertAlmostEqual(c1r1_c2r2_sum, 0.9, places=0)
+        c1r1_c2r2_average, gbest = optimizer.run_iteration(number=5)
+        self.assertAlmostEqual(c1r1_c2r2_average, 0.9, places=0)
         self.assertTrue(type(gbest), Tensor)
 
 
