9c168ab85067d9fab879d9f0b49f37b31433c235
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2022-01-25 13:57:10 +0000

    Add docstring and fix pyling errors

--- adaswarm/utils/metrics.py
+++ adaswarm/utils/metrics.py
@@ -1,14 +1,32 @@
+"""
+    A module for reporting metrics for training and
+    testing runs
+"""
 import os
 import time
 import pandas as pd
 
 
-class Metrics(object):
-    class Stats(object):
+class Metrics:
+    """
+    Metrics class to capture
+    * Time taken to run
+    * Accuracy
+    """
+
+    class Stats:
+        """
+        Class to hold the value of accuracy
+        """
+
         def __init__(self):
             self.best_accuracy = 0.0
 
         def update_train_accuracy(self, value):
+            """
+            Compare and store the best accuracy
+            value
+            """
             if value > self.best_accuracy:
                 self.best_accuracy = value
 
@@ -16,22 +34,24 @@ class Metrics(object):
         self.name = name
         self.accuracy = Metrics.Stats()
         self.name = name
+        self.tstart = time.time()
 
     def __enter__(self):
-        self.tstart = time.time()
         return self.accuracy
 
     def __exit__(self, *args):
 
-        time_taken = (time.time() - self.tstart)
-        this_summary_dataframe = pd.DataFrame({
-            "Run name":self.name, 
-            "Elapsed (seconds)":time_taken, 
-            "Training Accuracy":self.accuracy.best_accuracy}, 
-            index=[0])
+        time_taken = time.time() - self.tstart
+        this_summary_dataframe = pd.DataFrame(
+            {
+                "Run name": self.name,
+                "Elapsed (seconds)": time_taken,
+                "Training Accuracy": self.accuracy.best_accuracy,
+            },
+            index=[0],
+        )
 
         os.makedirs(os.path.dirname("tests/unit/output/"), exist_ok=True)
 
-        with open("tests/unit/output/summary.md", "w") as file:
+        with open("tests/unit/output/summary.md", mode="w", encoding="utf-8") as file:
             this_summary_dataframe.to_markdown(buf=file)
-
