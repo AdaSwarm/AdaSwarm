bc2609fcac5d16536db062bed058c848a69cfa80
Frank Kelly <frank.kelly@cantab.net>
2022-02-01 12:47:50 +0000

    Add test accuracy and loss to metrics (#56)
    * Change test functions to training specific
    
    * Add test loss
    
    * Add test accuracy to output

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -52,6 +52,15 @@ class TestMetrics(unittest.TestCase):
             metrics.update_training_accuracy(1.0)
             self.assertEqual(metrics.best_training_accuracy, 1.5)
 
+    def test_test_accuracy(self):
+        with Metrics(
+            md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME
+        ) as metrics:
+            metrics.update_test_accuracy(1.5)
+            metrics.update_test_accuracy(1.0)
+            self.assertEqual(metrics.best_test_accuracy, 1.5)
+
+
     def test_train_accuracy_md_output(self):
         with Metrics(
             md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME
@@ -62,6 +71,16 @@ class TestMetrics(unittest.TestCase):
             1.52,
         )
 
+    def test_test_accuracy_md_output(self):
+        with Metrics(
+            md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME
+        ) as metrics:
+            metrics.update_test_accuracy(0.01523)
+        self.assertEqual(
+            float(self.read_dataframe_from_markdown()["Test Accuracy %"].values[0]),
+            1.52,
+        )
+
     def test_train_accuracy_csv_output(self):
         with Metrics(
             md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME
@@ -69,6 +88,15 @@ class TestMetrics(unittest.TestCase):
             metrics.update_training_accuracy(0.01523)
         self.assertEqual(
             float(self.read_dataframe_from_csv()["Training Accuracy %"].values[0]), 1.52
+        )
+
+    def test_test_accuracy_csv_output(self):
+        with Metrics(
+            md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME
+        ) as metrics:
+            metrics.update_test_accuracy(0.01523)
+        self.assertEqual(
+            float(self.read_dataframe_from_csv()["Test Accuracy %"].values[0]), 1.52
         )
 
     def test_time_taken(self):
@@ -100,6 +128,23 @@ class TestMetrics(unittest.TestCase):
             float(self.read_dataframe_from_csv()["Training Accuracy %"].values[1]), 1.62
         )
 
+    def test_test_accuracy_csv_output(self):
+        with Metrics(
+            md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME
+        ) as metrics:
+            metrics.update_test_accuracy(0.01523)
+
+        with Metrics(
+            md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME
+        ) as metrics:
+            metrics.update_test_accuracy(0.01623)
+        self.assertEqual(
+            float(self.read_dataframe_from_csv()["Test Accuracy %"].values[0]), 1.52
+        )
+        self.assertEqual(
+            float(self.read_dataframe_from_csv()["Test Accuracy %"].values[1]), 1.62
+        )
+
     @freeze_time("2021-12-25 03:01:33")
     def test_start_time_in_output(self):
 
@@ -125,7 +170,7 @@ class TestMetrics(unittest.TestCase):
             2,
         )
 
-    def test_best_loss(self):
+    def test_best_training_loss(self):
 
         with Metrics(
             md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME
@@ -139,12 +184,35 @@ class TestMetrics(unittest.TestCase):
             0.1,
         )
 
-    def test_empty_best_loss(self):
+    def test_best_test_loss(self):
+        with Metrics(
+            md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME
+        ) as metrics:
+            metrics.update_test_loss(0.2)
+            metrics.update_test_loss(0.1)
+            metrics.update_test_loss(0.3)
+
+        self.assertEqual(
+            self.read_dataframe_from_csv()["Test Loss"].values[0],
+            0.1,
+        )
+
+    def test_empty_best_training_loss(self):
 
         with Metrics(md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME):
             pass
 
         self.assertEqual(
             self.read_dataframe_from_csv()["Training Loss"].values[0],
+            "Not set",
+        )
+
+    def test_empty_best_test_loss(self):
+
+        with Metrics(md_filepath=MD_OUTPUT_FILENAME, csv_filepath=CSV_OUTPUT_FILENAME):
+            pass
+
+        self.assertEqual(
+            self.read_dataframe_from_csv()["Test Loss"].values[0],
             "Not set",
         )
