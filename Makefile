e4268afb0c40994ca14884fa6368a55202e4a192
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2022-06-23 09:10:25 +0100

    Fix all linting errors

--- Makefile
+++ Makefile
@@ -1,6 +1,4 @@
 test:
 	poetry run python -m unittest tests/unit/test_* 
 lint:
-	poetry run pylint -d duplicate-code examples/main.py adaswarm/particle.py adaswarm/data.py \
-	adaswarm/model.py adaswarm/utils/matrix.py adaswarm/utils/options.py adaswarm/utils/progressbar.py \
-	adaswarm/rempso.py
+	poetry run pylint -d duplicate-code adaswarm/**/*.py
--- adaswarm/utils/__init__.py
+++ adaswarm/utils/__init__.py
@@ -1,12 +1,15 @@
+"""
+Utility for importing keras functions
+"""
 import numpy as np
 from adaswarm.utils.progressbar import *
 
 
-
+# pylint: disable=C0301
 # Please refer to Keras: https://github.com/keras-team/keras/blob/14f71177ad28a60a4ea41775b2ac159d3688c792/keras/utils/np_utils.py#L22-L74
 
 
-def to_categorical(y, num_classes=None, dtype="float32"):
+def to_categorical(y_array, num_classes=None, dtype="float32"):
     """Converts a class vector (integers) to binary class matrix.
 
     E.g. for use with `categorical_crossentropy`.
@@ -46,16 +49,16 @@ def to_categorical(y, num_classes=None, dtype="float32"):
     >>> print(np.around(loss, 5))
     [0. 0. 0. 0.]
     """
-    y = np.array(y, dtype="int")
-    input_shape = y.shape
+    y_array = np.array(y_array, dtype="int")
+    input_shape = y_array.shape
     if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
         input_shape = tuple(input_shape[:-1])
-    y = y.ravel()
+    y_array = y_array.ravel()
     if not num_classes:
-        num_classes = np.max(y) + 1
-    n = y.shape[0]
-    categorical = np.zeros((n, num_classes), dtype=dtype)
-    categorical[np.arange(n), y] = 1
+        num_classes = np.max(y_array) + 1
+    n_columns = y_array.shape[0]
+    categorical = np.zeros((n_columns, num_classes), dtype=dtype)
+    categorical[np.arange(n_columns), y_array] = 1
     output_shape = input_shape + (num_classes,)
     categorical = np.reshape(categorical, output_shape)
     return categorical
